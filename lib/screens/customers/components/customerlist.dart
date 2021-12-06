97b57a0ae52c8d3606bff626812c0adb4df7545d
ghost <akshaykc222@gmail.com>
2021-12-06 08:52:20 +0530

    dfdf

---
+++ lib/screens/customers/components/customerlist.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomerList extends StatelessWidget {
+  const CustomerList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:appBarWithSearch
+    );
+  }
+}
