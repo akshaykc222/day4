3495b91f907eb345d123be25331520424813d5b7
ghost <akshaykc222@gmail.com>
2021-12-14 12:15:58 +0530

    dfdf

---
+++ lib/screens/roles/provider/role_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:seed_sales/screens/roles/models/role_model.dart';
+
+class RoleProvider with ChangeNotifier{
+  List<Roles> roleList=[];
+
+  void addRoles(){
+
+  }
+}
